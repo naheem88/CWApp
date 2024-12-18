14c2351013214eaa5818d29b8867b4f3e833465f
naheem88 <naheempakeer@gmail.com>
2024-12-18 15:10:23 +0530

    Initial Commit

---
+++ CWApp/CWAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWAppApp.swift
+//  CWApp
+//
+//  Created by Thuan Naheem Pakeer on 2024-12-18.
+//
+
+import SwiftUI
+
+@main
+struct CWAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
